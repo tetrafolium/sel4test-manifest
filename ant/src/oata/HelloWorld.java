adc70dc4517570df19184b7f0212f6c08554662f
TE-ShomeiRyo <shomei.ryo@sony.com>
2025-09-18 14:35:40 +0900

    Add files via upload
    Signed-off-by: TE-ShomeiRyo <shomei.ryo@sony.com>

---
+++ ant/src/oata/HelloWorld.java
@@ -0,0 +1,9 @@
+package oata;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s = null;
+	s.length();
+        System.out.println("Hello World");
+    }
+}
